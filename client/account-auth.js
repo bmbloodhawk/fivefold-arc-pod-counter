969ccf5e9a803e6f492daf31199db6345f19b451
bmbloodhawk <bmbloodhawk@gmail.com>
2026-09-14 23:37:34 -0700

    Add optional email account sign-in

--- client/account-auth.js
+++ client/account-auth.js
@@ -25,3 +25,9 @@ export async function currentAccountToken() {
   const user = (await firebaseAuth()).currentUser;
   return user ? user.getIdToken() : null;
 }
+
+export async function emailAccountToken(email, password, create = false) {
+  const { createUserWithEmailAndPassword, signInWithEmailAndPassword } = await import("https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js");
+  const credential = create ? await createUserWithEmailAndPassword(await firebaseAuth(), email, password) : await signInWithEmailAndPassword(await firebaseAuth(), email, password);
+  return credential.user.getIdToken();
+}
